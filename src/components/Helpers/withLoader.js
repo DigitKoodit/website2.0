ddd48ebde24dd6b9f80db2ec29a87874e53c3462
niemisami <sami@nieminen.fi>
2018-06-02 08:13:41 +0300

    Add withLoader HOC which adds slight delay before sending isLoading prop

---
+++ src/components/Helpers/withLoader.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+const withLoader = (LoadingComponent, loaderDelay = 500) =>
+  class LoaderHoc extends Component {
+    state = {
+      willStartLoading: false,
+      isLoading: false
+    }
+
+    showLoader = () => {
+      // precaution if component is not mounted anymore
+      if(this.state.willStartLoading) {
+        this.setState(prevState => ({
+          isLoading: this.state.willStartLoading
+        }))
+      }
+      this.props.showLoader()
+    }
+
+    hideLoader = () => {
+      this.setState({
+        willStartLoading: false,
+        isLoading: false
+      })
+    }
+
+    startLoadDelay = loading => {
+      if(loading) {
+        this.setState({
+          willStartLoading: true
+        })
+        setTimeout(() => {
+          this.showLoader()
+        }, loaderDelay)
+      } else {
+        this.hideLoader()
+      }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+      if(prevProps.loading !== this.props.loading) {
+        this.startLoadDelay(this.props.loading)
+      }
+    }
+    static propTypes = {
+      loading: PropTypes.bool.isRequired,
+      showLoader: PropTypes.func
+    }
+
+    render() {
+      return (
+        <LoadingComponent {...this.props} isLoading={this.state.isLoading} />
+      )
+    }
+  }
+
+export default withLoader
