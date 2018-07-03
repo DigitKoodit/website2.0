39df6d84f4ae0e7ec629a02a298938df0e248bf3
niemisami <sami@nieminen.fi>
2018-07-03 22:52:24 +0300

    combine pending/error actions with all crud operations

--- src/reducers/siteNavigationReducers.js
+++ src/reducers/siteNavigationReducers.js
@@ -1,5 +1,7 @@
-import { createReducer } from '../store/helpers'
+import { crudTypes, createReducer } from '../store/helpers'
 import { types } from '../actions'
+import merge from 'lodash/merge'
+import mapValues from 'lodash/mapValues'
 
 const initialState = {
   records: [],
@@ -7,19 +9,36 @@ const initialState = {
   loading: false
 }
 
+const combineCrudOperations = (type, actionType, reducerFunc) =>
+  mapValues(crudTypes, crudOperation => ({
+    [type[crudOperation][actionType]]: reducerFunc
+  }))
+
 export default createReducer(initialState, {
-  [types.SITE_NAVIGATION.PENDING]: state => ({
+  ...combineCrudOperations(types.SITE_NAVIGATION, 'PENDING', state => ({
     ...state,
     loading: true
-  }),
-  [types.SITE_NAVIGATION.SUCCESS]: (state, action) => ({
+  })),
+  ...combineCrudOperations(types.SITE_NAVIGATION, 'ERROR', (state, action) => ({
+    ...state,
+    error: action.error,
+    loading: false
+  })),
+  [types.SITE_NAVIGATION.FETCH.SUCCESS]: (state, action) => ({
     ...state,
     records: action.response,
     loading: false
   }),
-  [types.SITE_NAVIGATION.ERROR]: (state, action) => ({
+  [types.SITE_NAVIGATION.UPDATE.SUCCESS]: (state, action) => ({
     ...state,
-    error: action.error,
+    // rough way of copying updated object to records
+    records: Array.isArray(action.response) ? action.response : merge(state.records, action.response),
+    loading: false
+  }),
+  [types.SITE_NAVIGATION.DELETE.SUCCESS]: (state, action) => ({
+    ...state,
+    // rough way of copying updated object to records
+    records: state.filter(record => record.id !== action.id),
     loading: false
   })
 })
