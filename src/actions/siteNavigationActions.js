84ba636181c8b3ab9aca7af7e12147b4b297cdc7
niemisami <sami@nieminen.fi>
2018-07-07 12:03:06 +0300

    add crud operations for nav items and parse subitems

--- src/actions/siteNavigationActions.js
+++ src/actions/siteNavigationActions.js
@@ -1,6 +1,7 @@
 import { actionKeys } from './actionTypes'
 import { crudTypes, createCrudTypes, createAction } from '../store/helpers'
 import createCrudService from '../services/createCrudService'
+import { displaySnackbar } from '../actions/uiActions'
 
 const navItemPublicCrud = createCrudService('/api/content/navigation')
 // Private routes require authorization header
@@ -18,7 +19,61 @@ const siteNavigationActions = {
         .then(response => {
           dispatch(this.success(response, crudTypes.FETCH))
         }).catch(err => {
-          console.error('Sivun navigointia ei voitu ladata', err)
+          console.error('Sivukarttaa ei voitu ladata', err)
+          dispatch(this.error({ common: 'Sisältö ei saataville' }, crudTypes.FETCH))
+        })
+    }
+  },
+  prepareNew() {
+    return dispatch => {
+      const dummyItem = { id: -Date.now(), title: '', path: '', subItems: [], parentId: null, isCustom: false, weight: 0, isVisible: false }
+      dispatch(this.success(dummyItem, crudTypes.CREATE))
+    }
+  },
+  addNavItem(navItem) {
+    return dispatch => {
+      dispatch(this.pending(crudTypes.CREATE))
+      navItemCrud.create(navItem)
+        .then(response => {
+          dispatch(this.success(response, crudTypes.CREATE))
+          navItem.id < 0 && dispatch(this.success(navItem, crudTypes.DELETE))
+          navItem.id < 0 && dispatch(displaySnackbar('Luominen onnistui'))
+        }).catch(err => {
+          console.error('Navigaation lisäys onnistui', err)
+          dispatch(this.error({ common: 'Navigaation lisäys epäonnistui' }, crudTypes.CREATE))
+          navItem.id < 0 && dispatch(displaySnackbar('Luominen epäonnistui'))
+        })
+    }
+  },
+  updateNavigation(navItem) {
+    return dispatch => {
+      dispatch(this.pending(crudTypes.UPDATE))
+      navItemCrud.update(navItem)
+        .then(response => {
+          dispatch(this.success(response, crudTypes.UPDATE))
+          dispatch(displaySnackbar('Tallennus onnistui'))
+        }).catch(err => {
+          console.error('Navigaation päivitys epäonnistui', err)
+          dispatch(this.error({ common: 'Navigaation päivitys epäonnistui' }, crudTypes.UPDATE))
+          dispatch(displaySnackbar('Tallennus epäonnistui'))
+        })
+    }
+  },
+  removeNavItem(navItem) {
+    return dispatch => {
+      // User wants to remove unsaved data
+      if(navItem.id < 0) {
+        return dispatch(this.success(navItem, crudTypes.DELETE))
+      }
+      dispatch(this.pending(crudTypes.DELETE))
+      navItemCrud.performDelete(navItem)
+        .then(response => {
+          dispatch(this.success(navItem, crudTypes.DELETE))
+          dispatch(displaySnackbar('Poistaminen onnistui'))
+        }).catch(err => {
+          console.error('Elementin poisto epäonnistui', err)
+          dispatch(this.error({ common: 'Elementin poisto epäonnistui' }, crudTypes.DELETE))
+          dispatch(displaySnackbar('Poistaminen epäonnistui'))
         })
     }
   }
--- src/reducers/siteNavigationReducers.js
+++ src/reducers/siteNavigationReducers.js
@@ -1,6 +1,8 @@
 import { crudTypes, createReducer } from '../store/helpers'
 import { types } from '../actions'
-import merge from 'lodash/merge'
+import toArray from 'lodash/toArray'
+import sortBy from 'lodash/sortBy'
+import includes from 'lodash/includes'
 import mapValues from 'lodash/mapValues'
 
 const initialState = {
@@ -9,36 +11,58 @@ const initialState = {
   loading: false
 }
 
-const combineCrudOperations = (type, actionType, reducerFunc) =>
+const combineCrudOperationReducers = (type, actionType, reducerFunc) =>
   mapValues(crudTypes, crudOperation => ({
     [type[crudOperation][actionType]]: reducerFunc
   }))
 
 export default createReducer(initialState, {
-  ...combineCrudOperations(types.SITE_NAVIGATION, 'PENDING', state => ({
+  ...combineCrudOperationReducers(types.SITE_NAVIGATION, 'PENDING', state => ({
     ...state,
     loading: true
   })),
-  ...combineCrudOperations(types.SITE_NAVIGATION, 'ERROR', (state, action) => ({
+  ...combineCrudOperationReducers(types.SITE_NAVIGATION, 'ERROR', (state, action) => ({
     ...state,
     error: action.error,
     loading: false
   })),
   [types.SITE_NAVIGATION.FETCH.SUCCESS]: (state, action) => ({
     ...state,
-    records: action.response,
+    records: parseSubItems([...action.response]),
+    loading: false
+  }),
+  [types.SITE_NAVIGATION.CREATE.SUCCESS]: (state, action) => ({
+    ...state,
+    // rough way of copying updated object to records
+    records: parseSubItems([...state.records, action.response]),
     loading: false
   }),
   [types.SITE_NAVIGATION.UPDATE.SUCCESS]: (state, action) => ({
     ...state,
     // rough way of copying updated object to records
-    records: Array.isArray(action.response) ? action.response : merge(state.records, action.response),
+    records: parseSubItems(state.records.map(item => item.id === action.response.id ? action.response : item)),
     loading: false
   }),
   [types.SITE_NAVIGATION.DELETE.SUCCESS]: (state, action) => ({
     ...state,
     // rough way of copying updated object to records
-    records: state.filter(record => record.id !== action.id),
+    records: state.records.filter(item => item.id !== action.response.id),
     loading: false
   })
 })
+
+const parseSubItems = navItems => {
+  const navigation = [...navItems]
+    .reduce((acc, item) => ({
+      ...acc,
+      [item.id]: { ...item, subItems: [] }
+    }), {})
+  const hasParent = sortBy(navItems.filter(item => item.parentId), ['weight'])
+  hasParent.forEach(item => {
+    const parent = navigation[item.parentId]
+    if(parent && !includes(parent.subItems, item.id)) {
+      navigation[item.parentId] = { ...parent, subItems: [...parent.subItems || [], item.id] }
+    }
+  })
+  return sortBy(toArray(navigation), ['weight'])
+}
