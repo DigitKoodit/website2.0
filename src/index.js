459eb1d67f1cbe500d4c29ee20b0115619470515
niemisami <sami@nieminen.fi>
2018-07-31 22:08:23 +0300

    Import Bulma sass files independently
    Opt out tag.sass which caused problems with Markdown rendering

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import thunk from 'redux-thunk'
 import App from './containers/App'
 import configureStore from './store/configureStore'
 
-import 'bulma/css/bulma.min.css'
+import './styles/bulma.css'
 import './styles/index.css'
 
 const history = createHistory()
