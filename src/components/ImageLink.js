0836bcee07abbf4873718f93021e03a2e4c427dd
miipel <mpeltotalo@gmail.com>
2018-08-14 21:35:59 +0300

    Tweaked sponsorView to show up nicely

--- src/components/ImageLink.js
+++ src/components/ImageLink.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 // Set rel value to prevent reverse tabnabbing https://github.com/asciidoctor/asciidoctor/issues/2071
 const ImageLink = ({ name, link, imageUrl, target = '_blank' }) => (
   <a href={link} alt={name} target={target} rel={target === '_blank' ? 'noopener noreferrer' : ''}>
-    <img src={imageUrl} alt={name} />
+    <img style={{ maxHeight: '75px' }} src={imageUrl} alt={name} />
   </a>
 )
 
