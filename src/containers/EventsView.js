b4ea9af03107da22fb101a0f677070300672523d
Konsta Purtsi <konsta.purtsi@vincit.fi>
2018-08-14 23:03:17 +0300

    created eventsView, added prop-types to package.json

--- src/containers/EventsView.js
+++ src/containers/EventsView.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react'
 import { getCalendarEventsShort } from '../lib/googleUtils'
-import { Content, Tile, Box } from 'bloomer'
+import { Box, Column, Columns, Content, Tile, Title } from 'bloomer'
+import PropTypes from 'prop-types'
+import * as _ from 'lodash'
+import moment from 'moment'
+import 'moment/locale/fi'
 
 class EventsView extends Component {
+  state = {
+    events: null
+  }
+
   componentDidMount() {
     getCalendarEventsShort()
       .then(events => {
-        console.log('TODO display events', events)
+        this.setState({ events })
       })
       .catch(error => {
         console.log('ERROR FETHCING CALENDAR EVENTS', error)
       })
   }
+
   render() {
+    const firstThreeDays = _(this.state.events)
+      .toArray()
+      .take(3)
+      .value()
+
+    // TODO: Add calendar button
     return (
-      <Tile isParent style={{ padding: 30 }}>
+      <Tile isParent style={{padding: 30}}>
         <Tile isChild render={
           props => (
             <Box {...props}>
               <Content>
-                Tapahtumakalenteri
+                <Title>Tapahtumat</Title>
+                <Columns>
+                  {firstThreeDays.map(renderDay)}
+                </Columns>
               </Content>
             </Box>
           )
@@ -27,6 +45,43 @@ class EventsView extends Component {
       </Tile>
     )
   }
+}
+
+
+const renderDay = (events) => (
+  <Column isSize={{ default: '1/3', mobile: '1' }}>
+    <div className='is-size-5 pb-2'>
+      {moment(events[0].start).format('dddd DD.MM.')}
+    </div>
+    {events.map(renderEvent)}
+  </Column>
+)
+
+
+const renderEvent = ({ start, end, title, location }) => {
+  const formattedStart = moment(start).format('HH:mm')
+  const formattedEnd = moment(end).format('HH:mm')
+  return (
+    <div className='pb-2'>
+      <span className='has-text-weight-semibold'>{title}</span><br />
+      <span className='has-text-grey-light has-text-weight-bold'>{`${formattedStart} - ${formattedEnd}`}</span><br />
+      {location && <span className='has-text-grey is-size-6'>{location}</span>}
+    </div>
+  )
+}
+
+renderEvent.propTypes = {
+  // start time of event
+  start: PropTypes.object,
+
+  // end time of event
+  end: PropTypes.object,
+
+  // event title
+  title: PropTypes.string,
+
+  // event location
+  location: PropTypes.string
 }
 
 export default EventsView
--- src/lib/googleUtils.js
+++ src/lib/googleUtils.js
@@ -1,29 +1,38 @@
 import { get } from '../api/apiHelper'
+import moment from 'moment'
 
 const CALENDAR_ID = '51n4vtv46paes63es2ddea3vgg%40group.calendar.google.com'
 const API_KEY = 'AIzaSyBE5UIEKN3IZQkHwBfpHlCk4EA47BJizPU'
 // TODO: limit Google Calendar access to certain domain from https://console.developers.google.com/apis/
 
-const EVENT_COUNT = 4
+const EVENT_COUNT = 12
 const dateMax = new Date()
 // Fetch next 4 events but not more than six months from now
-dateMax.setMonth(new Date().getMonth() + 6)
+dateMax.setMonth(new Date().getMonth() + 3)
 const TIME_MIN = new Date().toISOString()
 const TIME_MAX = dateMax.toISOString()
-const shortEventsUrl = `https://content.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?alwaysIncludeEmail=false&maxResults=${EVENT_COUNT}&timeMin=${TIME_MIN}&timeMax=${TIME_MAX}&showDeleted=false&showHiddenInvitations=false&singleEvents=true&key=${API_KEY}`
+const shortEventsUrl = `https://content.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?alwaysIncludeEmail=false&maxResults=${EVENT_COUNT}&timeMin=${TIME_MIN}&timeMax=${TIME_MAX}&showDeleted=false&showHiddenInvitations=false&singleEvents=true&key=${API_KEY}&orderBy=startTime`
 
-const dateMin = new Date()
-dateMin.setMonth(new Date().getMonth() - 12)
-const CALENDAR_TIME_MIN = dateMin.toISOString()
-const allEventsUrl = `https://content.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?&timeMin=${CALENDAR_TIME_MIN}&timeMax=${TIME_MAX}&showDeleted=false&showHiddenInvitations=false&singleEvents=true&key=${API_KEY}`
+const allEventsUrl = `https://content.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?&timeMin=${TIME_MIN}&timeMax=${TIME_MAX}&showDeleted=false&showHiddenInvitations=false&singleEvents=true&key=${API_KEY}&orderBy=startTime`
+
+const eventsIntoTheeDayGroups = (events) => {
+  return events
+    .sort((a, b) => moment(a.start).diff(moment(b.start)))
+    .reduce((acc, event) => {
+      const key = moment(event.start).format('YYYY-MM-DD')
+      const oldEvents = acc[key] || []
+      return {
+        ...acc,
+        [key]: [...oldEvents, event]
+      }
+    }, {})
+}
 
 export const getCalendarEventsShort = () =>
   new Promise((resolve, reject) => {
     get(shortEventsUrl)
       .then(parseCalendarDataSimple) // parse data received from Axios api call
-      .then(events => {
-        return resolve(events)
-      })
+      .then(events => resolve(eventsIntoTheeDayGroups(events)))
       .catch(reason => {
         return reject(reason.response || reason)
       })
@@ -47,9 +56,11 @@ const parseCalendarDataSimple = data =>
     const events = data.items.map((event) => ({
       start: new Date(event.start.date || event.start.dateTime),
       end: new Date(event.end.date || event.end.dateTime),
-      title: event.summary
+      title: event.summary,
+      description: event.description,
+      location: event.location
     }))
-    if(events && events.length > 0) {
+    if (events && events.length > 0) {
       return resolve(events)
     }
     throw new Error('No calendar events')
