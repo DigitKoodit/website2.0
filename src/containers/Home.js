9ecc4cc7a099ad9d10e1f11e534e51246d7bf57e
Miika Peltotalo <mpeltotalo@gmail.com>
2018-08-21 15:28:53 +0300

    Social media feed is currently under construction

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,34 +1,38 @@
-import React, { Fragment } from 'react'
-import Helmet from 'react-helmet'
-import WelcomeView from '../components/WelcomeView'
-import EventView from './EventsView'
-import SponsorsView from './SponsorsView'
-import SocialMediaView from './SocialMediaView'
-import SocialMediaFeed from './SocialMediaFeed'
-import heroImage from '../public/images/hero-16x9.jpg'
-import { Hero, HeroBody, Tile, Section } from 'bloomer'
+import React, { Fragment } from "react";
+import Helmet from "react-helmet";
+import WelcomeView from "../components/WelcomeView";
+import EventView from "./EventsView";
+import SponsorsView from "./SponsorsView";
+import SocialMediaView from "./SocialMediaView";
+import SocialMediaFeed from "./SocialMediaFeed";
+import heroImage from "../public/images/hero-16x9.jpg";
+import { Hero, HeroBody, Tile, Section } from "bloomer";
 
-const heroBgStyle = { backgroundImage: `url(${heroImage})` }
+const heroBgStyle = { backgroundImage: `url(${heroImage})` };
 const Home = () => {
   return (
     <Fragment>
       <Helmet>
         <title>Digit ry</title>
       </Helmet>
-      <Hero isSize='small' className='is-halfheight'>
-        <HeroBody isPaddingless className='has-bg-img' style={heroBgStyle} />
+      <Hero isSize="small" className="is-halfheight">
+        <HeroBody isPaddingless className="has-bg-img" style={heroBgStyle} />
       </Hero>
-      <Section className='pt-4'>
+      <Section className="pt-4">
         <Tile isAncestor>
           <WelcomeView />
           <EventView />
         </Tile>
       </Section>
-      {/* <SocialMediaFeed /> */}
+      <Section className="pt-4">
+        <Tile isAncestor>
+          <SocialMediaFeed />
+        </Tile>
+      </Section>
       <SponsorsView />
       <SocialMediaView />
-    </Fragment >
-  )
-}
+    </Fragment>
+  );
+};
 
-export default Home
+export default Home;
--- src/containers/SocialMediaFeed.js
+++ src/containers/SocialMediaFeed.js
@@ -1,7 +1,55 @@
-import React, { Component } from 'react'
-import FacebookProvider, { EmbeddedPost } from 'react-facebook'
-import { Media, MediaLeft, MediaRight, MediaContent, Content, Level, LevelLeft, LevelItem, Icon, Delete, Image } from 'bloomer'
+import React, { Component } from "react";
+import FacebookProvider, { EmbeddedPost } from "react-facebook";
+import { Tile, Box } from "bloomer";
 
-export default class Example extends Component {
+class SocialMediaView extends Component {
+  loadFbFeedApi() {
+    window.fbAsyncInit = function() {
+      window.FB.init({
+        appId: "225172908136009",
+        xfbml: true, // parse social plugins on this page
+        version: "v3.1"
+      });
+      window.FB.api(
+        "/digitry/posts",
+        'GET',
+        {access_token: "EAADMyycz1kkBAAiRtKTBjzSfk6LXSNRO6gyKZA10WEjk0mb6ezC0Ca78dhYBcqnyusQXoSjBjgbzZAHUtkJzY3qZAEBsNZAScN1VpEBZCkkBRZCFRf8pp5TdF1MLXDHTLX7MS2EDN9bjXvOeF20j2ZAVO4YUSumJQN3ZBQo0hFJCCANHlgUZCq6s3dSyfECeI9YYZD"},
+        function(response) {
+            
+        }
+      )}
+    }
 
+    console.log("Loading fb api");
+    // Load the SDK asynchronously
+    (function(d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  }
+
+  componentDidMount() {
+    this.loadFbFeedApi();
+  }
+
+  render() {
+    return (
+      <Tile isParent isSize={12} style={{ padding: 30 }}>
+        <Tile isChild render={props => <Box {...props}>{/*  */}</Box>} />
+      </Tile>
+    );
+  }
 }
+
+// const SocialMediaView = () => (
+//   <Tile isParent isSize={12} style={{ padding: 30 }}>
+//     <Tile isChild render={props => <Box {...props}>{/*  */}</Box>} />
+//   </Tile>
+// );
+
+export default SocialMediaView;
