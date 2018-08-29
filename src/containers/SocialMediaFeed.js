ecad43c6566f608771900dc18f6d496d87f85303
Miika Peltotalo <mpeltotalo@gmail.com>
2018-08-29 13:21:51 +0300

    Completed social media feed for Facebook

--- src/containers/SocialMediaFeed.js
+++ src/containers/SocialMediaFeed.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import { Tile, Box, Media, MediaLeft, Image, Content, Title, MediaContent } from 'bloomer'
+import { Box, Media, MediaLeft, Content, Title, MediaContent, Tile } from 'bloomer'
 import moment from 'moment'
 import 'moment/locale/fi'
 
+import ReactMarkdown from 'react-markdown'
 import fbFeed from './SocialMediaFeed.json'
 
 class SocialMediaFeed extends Component {
@@ -23,45 +24,43 @@ class SocialMediaFeed extends Component {
 
   render() {
     const posts = this.state.feed.posts.data
-    const page = this.state.feed.page.picture.data
+    const pagePicture = this.state.feed.page.picture.data
 
     return (
-      <Tile isAncestor>
-        <Tile isParent isSize={4} style={{ padding: 30 }}>
-          <Tile isChild render={
-            props => (
-              <Box {...props}>
-                {posts && (
-                  posts.map(post => (
-                    <Media>
-                      <MediaLeft>
-                        <Image src={page.url} />
-                      </MediaLeft>
-                      <MediaContent>
-                        <Content>
-                          <p>
-                            <strong>Digit ry</strong>
-                            <small> {moment().startOf(post.current_time).fromNow()}</small>
-                            <div key={post.id}>
-                              <Content>
-                                <Title isSize={4} >{post.name}</Title>
-                                <p>{post.description}</p>
-                              </Content>
-                              {/* <Box>
-                                <Image src={post.picture} />
-                              </Box> */}
-                            </div>
-                          </p>
-                        </Content>
-                      </MediaContent>
-                    </Media>
-                  ))
-                )}
-              </Box>
-            )
-          } />
-        </Tile>
-      </Tile>
+      <Tile isSize={4} isParent style={{ padding: 30 }}>
+        <Tile isChild render={
+          props => (
+            <Box {...props} className='is-centered' style={{ overflowX: 'auto', height: '550px' }}>
+              {posts && (
+                posts.map(post => (
+                  <Media key={post.id}>
+                    <MediaLeft>
+                      <figure className='image'>
+                        <img className='is-rounded' src={pagePicture.url} alt='Digit' />
+                      </figure>
+                    </MediaLeft>
+                    <MediaContent>
+                      <Content>
+                        <strong>Digit ry @Facebook</strong>
+                        <br />
+                        <small>{moment(post.created_time).from(moment())}</small>
+                        <Title isSize={5} >{post.name}</Title>
+                        <ReactMarkdown
+                          className={'markdown-area'}
+                          source={decodeURI(post.description)}
+                          escapeHtml={false}
+                        />
+                        {/* <Image isRatio='2:1' src={post.picture} /> */}
+                      </Content>
+                    </MediaContent>
+                  </Media>
+                ))
+              )}
+            </Box>
+
+          )
+        } />
+      </Tile >
     )
   }
 }
