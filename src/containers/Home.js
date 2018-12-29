88c31464be76a7a0aeb96cd2a7e119c391aa775c
niemisami <sami@nieminen.fi>
2018-12-29 16:40:04 +0200

    Show social media icons on production and wrap sponsors in card

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import Helmet from 'react-helmet'
 import WelcomeView from '../components/WelcomeView'
 import EventView from './EventsView'
@@ -6,36 +6,46 @@ import SponsorsView from './SponsorsView'
 import SocialMediaView from './SocialMediaView'
 import SocialMediaFeed from './SocialMediaFeed'
 import heroImage from '../public/images/hero-16x9.jpg'
-import { Hero, HeroBody, Tile, Section } from 'bloomer'
+import { Hero, HeroBody, Tile, Section, Columns, Column } from 'bloomer'
 
 const heroBgStyle = { backgroundImage: `url(${heroImage})` }
 const isDev = process.env.NODE_ENV === 'development'
 const Home = () => {
   return (
-    <Fragment>
+    <>
       <Helmet>
         <title>Digit ry</title>
       </Helmet>
       <Hero isSize='small' className='is-halfheight'>
         <HeroBody isPaddingless className='has-bg-img' style={heroBgStyle} />
       </Hero>
-      <Section className='pt-4'>
-        <Tile isAncestor>
-          <WelcomeView />
-          <EventView />
-        </Tile>
-      </Section>
-      {isDev && <>
-        <Section className='pt-4'>
-          <Tile isAncestor>
-            <SocialMediaFeed />
-          </Tile>
-        </Section>
-        <SponsorsView />
-        <SocialMediaView />
-      </>
-      }
-    </Fragment>
+      <Columns isCentered>
+        <Column isSize={{ mobile: 'full', desktop: '3/4', widescreen: '2/3' }} >
+          <Section className='p-4'>
+            <Tile isAncestor>
+              <WelcomeView />
+              <EventView />
+            </Tile>
+          </Section>
+          {isDev
+            ? <Section className='p-4 pt-0'>
+              <Tile isAncestor>
+                <SocialMediaFeed />
+                <SponsorsView />
+              </Tile>
+            </Section>
+            : <Section className='p-4 pt-0'>
+              <Tile isAncestor>
+                <SponsorsView />
+              </Tile>
+            </Section>
+          }
+          <Section className='p-4'>
+            <SocialMediaView />
+          </Section>
+        </Column>
+      </Columns>
+    </>
   )
 }
 
