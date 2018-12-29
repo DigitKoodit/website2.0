ce792f78667b8ea043af145a1365fc88b140e4dd
Konsta Purtsi <kovipu@utu.fi>
2018-12-29 16:32:12 +0200

    Changes to eventview. Still requires refactoring.

--- src/lib/googleUtils.js
+++ src/lib/googleUtils.js
@@ -9,30 +9,17 @@ const EVENT_COUNT = 12
 const dateMax = new Date()
 // Fetch next 4 events but not more than six months from now
 dateMax.setMonth(new Date().getMonth() + 3)
-const TIME_MIN = new Date().toISOString()
+const TIME_MIN = moment().startOf('day').toISOString()
 const TIME_MAX = dateMax.toISOString()
 const shortEventsUrl = `https://content.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?alwaysIncludeEmail=false&maxResults=${EVENT_COUNT}&timeMin=${TIME_MIN}&timeMax=${TIME_MAX}&showDeleted=false&showHiddenInvitations=false&singleEvents=true&key=${API_KEY}&orderBy=startTime`
 
 const allEventsUrl = `https://content.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?&timeMin=${TIME_MIN}&timeMax=${TIME_MAX}&showDeleted=false&showHiddenInvitations=false&singleEvents=true&key=${API_KEY}&orderBy=startTime`
 
-const eventsIntoTheeDayGroups = (events) => {
-  return events
-    .sort((a, b) => moment(a.start).diff(moment(b.start)))
-    .reduce((acc, event) => {
-      const key = moment(event.start).format('YYYY-MM-DD')
-      const oldEvents = acc[key] || []
-      return {
-        ...acc,
-        [key]: [...oldEvents, event]
-      }
-    }, {})
-}
-
 export const getCalendarEventsShort = () =>
   new Promise((resolve, reject) => {
     get(shortEventsUrl)
       .then(parseCalendarDataSimple) // parse data received from Axios api call
-      .then(events => resolve(eventsIntoTheeDayGroups(events)))
+      .then(events => resolve(events))
       .catch(reason => {
         return reject(reason.response || reason)
       })
@@ -52,10 +39,11 @@ export const getCalendarEvents = () =>
 
 // Returns a promise fullfilled with
 const parseCalendarDataSimple = data =>
-  new Promise((resolve, reject) => {
+  new Promise((resolve) => {
     const events = data.items.map((event) => ({
       start: new Date(event.start.date || event.start.dateTime),
       end: new Date(event.end.date || event.end.dateTime),
+      isAllDay: !!event.end.date,
       title: event.summary,
       description: event.description,
       location: event.location
