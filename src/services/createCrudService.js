17f997d74d453fe33aa7e66447887e1f8d62879c
niemisami <sami@nieminen.fi>
2019-01-16 07:42:39 +0200

    Update crud generator to allow additional parameters

--- src/services/createCrudService.js
+++ src/services/createCrudService.js
@@ -4,21 +4,22 @@ import { get, post, put, del } from '../api/apiHelper'
 const addBodyPayload = data => ({ body: JSON.stringify(data) })
 
 const createCrudService = (baseUrl, requireAuth = false) => {
-  const baseUrlTemplate = urlTemplate.parse(`${baseUrl}/{id}`)
-  const fetchAll = () =>
-    get(baseUrl, null, requireAuth)
+  const baseUrlTemplate = urlTemplate.parse(baseUrl)
+  const baseUrlIdTemplate = urlTemplate.parse(`${baseUrl}/{id}`)
+  const fetchAll = (params) =>
+    get(baseUrlTemplate.expand(params), null, requireAuth)
 
-  const fetchById = id =>
-    get(baseUrlTemplate.expand({ id }), null, requireAuth)
+  const fetchById = (id, params) =>
+    get(baseUrlIdTemplate.expand({ ...params, id }), null, requireAuth)
 
-  const create = data =>
-    post(baseUrlTemplate.expand({}), addBodyPayload(data), requireAuth)
+  const create = (data, params) =>
+    post(baseUrlIdTemplate.expand(params), addBodyPayload(data), requireAuth)
 
-  const update = data =>
-    put(baseUrlTemplate.expand({ id: data.id }), addBodyPayload(data), requireAuth)
+  const update = (data, params) =>
+    put(baseUrlIdTemplate.expand({ ...params, id: data.id }), addBodyPayload(data), requireAuth)
 
-  const performDelete = data =>
-    del(baseUrlTemplate.expand({ id: data.id }), null, requireAuth)
+  const performDelete = (data, params) =>
+    del(baseUrlIdTemplate.expand({ ...params, id: data.id }), null, requireAuth)
 
   return {
     fetchAll,
