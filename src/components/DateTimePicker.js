9eb16dc245241334fd4bd244b45f27ef4f36cc71
niemisami <sami@nieminen.fi>
2019-02-24 16:12:08 +0200

    Replace old datepicker with material-ui-picker
    Has good looking date time picker

---
+++ src/components/DateTimePicker.js
@@ -0,0 +1,43 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import moment from 'moment'
+import { MuiPickersUtilsProvider, InlineDateTimePicker } from 'material-ui-pickers'
+import MomentUtils from '@date-io/moment'
+
+const dateTimePickerLocale = 'fi'
+
+class DateTimePicker extends PureComponent {
+  handleDateChange = date => {
+    this.props.onChange(date ? date.format() : null)
+  }
+  render() {
+    const { selectedDate } = this.props
+    const dateObject = selectedDate ? moment(selectedDate) : null
+    return (
+      <>
+        <div className='picker'>
+          <MuiPickersUtilsProvider utils={MomentUtils} locale={dateTimePickerLocale}>
+            <InlineDateTimePicker
+              keyboard
+              ampm={false}
+              value={dateObject}
+              onChange={this.handleDateChange}
+              onError={console.log}
+              clearable
+              invalidDateMessage='Viallinen aika'
+              showTodayButton
+              format='DD.MM.YYYY HH:mm'
+            />
+          </MuiPickersUtilsProvider>
+        </div>
+      </>
+    )
+  }
+  static propTypes = {
+    selectedDate: PropTypes.object,
+    onChange: PropTypes.func.isRequired
+
+  }
+}
+
+export default DateTimePicker
