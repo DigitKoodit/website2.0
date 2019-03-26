25955d8fb1fc581f5cab34706cae472bf60b1cff
niemisami <sami@nieminen.fi>
2019-03-26 21:24:08 +0200

    Update new data grid to participant page and add export to CSV

--- src/containers/Enroll/ParticipantPage.js
+++ src/containers/Enroll/ParticipantPage.js
@@ -2,58 +2,93 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import isObject from 'lodash/isObject'
 import { connect } from 'react-redux'
+import memoizeOne from 'memoize-one'
 import { Columns, Column, Subtitle, Box, Media, MediaContent, Button } from 'bloomer'
 import { findEventEnrollsByEventId } from '../../selectors/eventEnrollSelectors'
 import DataGrid from '../../components/DataGrid'
+import { mapToDataGrid } from '../../components/dataGridHelper'
 import { eventEnrollActions } from '../../actions'
 import Modal from '../../components/Modal'
+import { toCsv, toCsvDataUri } from '../../helpers/helpers'
+import ExternalLinkButton from '../../components/Helpers/ExtrenalLinkButton'
+import eventPropTypes from './eventPropTypes'
+import { findEventById } from '../../selectors/eventSelectors'
+// import ExternalLinkButton from '../../components/Helpers/ExtrenalLinkButton';
 
-const defaultColumnProperties = {
-  width: 150,
-  sortable: true,
-  resizable: true
+const commitChanges = rows => ({ added, changed, deleted }) => {
+  if(added) {
+    const startingAddedId = rows.length > 0 ? rows[rows.length - 1].id + 1 : 0
+    rows = [
+      ...rows,
+      ...added.map((row, index) => ({
+        id: startingAddedId + index,
+        ...row
+      }))
+    ]
+  }
+  if(changed) {
+    rows = rows.map(row => (changed[row.id] ? { ...row, ...changed[row.id] } : row))
+  }
+  if(deleted) {
+    const deletedSet = new Set(deleted)
+    rows = rows.filter(row => !deletedSet.has(row.id))
+  }
+  console.log(rows)
+  return rows
 }
-
-const mapEnrollValues = enroll => ({
+const flattenEnrollValues = enroll => ({
   id: enroll.id,
   ...enroll.values,
-  isSpare: enroll.isSpare ? 'X' : ''
+  isSpare: enroll.isSpare ? 'Kyllä' : ''
 })
 
-const ObjectFormatter = ({ value }) => Object.entries(value)
+const ObjectFormatter = value => Object.entries(value)
   .filter(([key, value]) => !!value)
   .map(([key, value]) => `${key}, `)
 export class ParticipantPage extends PureComponent {
   state = {
-    selectedItemId: null
+    selectedItemId: null,
+    csvDataUri: null
   }
   componentDidMount = () => {
-    this.props.fetchEnrolls(this.props.eventId)
+    this.props.fetchEnrolls(this.props.event.id)
   }
 
-  mapFieldsToColumns = enroll => [
-    { key: 'id', name: 'ID', width: 45 },
+  mapFieldsToColumnsSpecs = enroll => mapToDataGrid([
+    { name: 'id', title: 'ID', width: 60, editingEnabled: false },
     ...Object.entries(enroll.values).map(([key, value]) => ({
-      key, name: key, sortable: true, filterable: true, formatter: isObject(value) ? ObjectFormatter : null
+      name: key, title: key, customRenderer: isObject(value) ? ObjectFormatter : null, wordWrapEnabled: true
     })),
-    { key: 'isSpare', name: 'Varasijalla' },
-    { key: 'delete', name: 'Poista', sortable: false, formatter: this.renderDeleteButton, width: 70 }
-  ].map(c => ({ ...defaultColumnProperties, ...c }))
+    { name: 'isSpare', title: 'varasijalla', customRenderer: (value) => value ? 'Kyllä' : '' }
+  ])
 
-  renderDeleteButton = value => (
-    <Button isColor='danger' isSize='small' isOutlined onClick={() => this.selectItem(value)}>
-      X
-    </Button>
-  )
+  mapCsvFields = enroll => [
+    'id',
+    ...Object.keys(enroll.values).map(key => key),
+    'isSpare'
+  ]
 
   selectItem = ({ row }) => this.setState({ selectedItemId: row.id })
   clearSelection = () => this.setState({ selectedItemId: null })
+  resetCsvData = () => this.setState({ csvDataUri: null })
   removeEventEnroll = () => {
     const { selectedItemId } = this.state
-    const { eventId, enrolls, removeEventEnroll } = this.props
+    const { event, enrolls, removeEventEnroll } = this.props
     const toBeRemovedEnroll = enrolls.find(enroll => enroll.id === selectedItemId)
-    removeEventEnroll(toBeRemovedEnroll, eventId)
+    removeEventEnroll(toBeRemovedEnroll, event.id)
     this.clearSelection()
+    this.resetCsvData()
+  }
+  generateCsv = () => {
+    toCsv(
+      this.mapCsvFields(this.props.enrolls[0]),
+      this.props.enrolls.map(flattenEnrollValues))
+      .then(csv => {
+        this.setState({ csvDataUri: toCsvDataUri(csv) })
+      })
+      .catch(error => {
+        console.error('CSV parse error', error)
+      })
   }
   render() {
     const { enrolls } = this.props
@@ -66,20 +101,30 @@ export class ParticipantPage extends PureComponent {
               <Media>
                 <MediaContent>
                   <p>Haluatko poistaa osallistujan tapahtumasta?</p>
-                  <Button isSize='small' isColor='danger' onClick={this.removeEventEnroll}>Kyllä</Button>
-                  <Button isSize='small' isOutlined onClick={this.clearSelection}>Peruuta</Button>
+                  <Button
+                    isSize='small'
+                    isColor='danger'
+                    onClick={this.removeEventEnroll}>
+                    Kyllä
+                  </Button>
+                  <Button
+                    isSize='small'
+                    isOutlined onClick={this.clearSelection}>
+                    Peruuta
+                  </Button>
                 </MediaContent>
               </Media>
             </Box>
           </Modal>
         }
         <Column>
-          <Subtitle isSize={5}>Osallistujat <small className='has-text-grey-light'>({enrolls.length})</small></Subtitle>
+          <Subtitle isSize={5}>Osallistujat <small className='has-text-grey-light'>({enrolls.length})</small>
+          </Subtitle>
           {enrolls.length
             ? <DataGrid
-              columns={this.mapFieldsToColumns(enrolls[0])}
-              initialRows={enrolls.map(mapEnrollValues)}
-              minHeight={600}
+              columnSpecs={this.mapFieldsToColumnsSpecs(enrolls[0])}
+              rows={enrolls.map(flattenEnrollValues)}
+              onCommitChanges={commitChanges(enrolls)}
             />
             : (
               <p className='has-text-grey mb-1'>
@@ -87,12 +132,29 @@ export class ParticipantPage extends PureComponent {
               </p>
             )
           }
+          <div className='mt-3'>
+            {this.renderExportButton()}
+          </div>
         </Column>
       </Columns>
     )
   }
+  renderExportButton = () => {
+    const { event, enrolls } = this.props
+    const { csvDataUri } = this.state
+    return csvDataUri
+      ? <ExternalLinkButton
+        isColor='primary'
+        href={csvDataUri}
+        filename={`export-${event.name}.csv`} >
+        <i className='fa fa-download' aria-hidden='true' />&nbsp;Lataa CSV
+      </ExternalLinkButton>
+      : <Button disabled={!enrolls.length} onClick={this.generateCsv} >
+        <i className='fa fa-cogs has-text-grey' aria-hidden='true' />&nbsp;Luo CSV
+      </Button>
+  }
   static propTypes = {
-    eventId: PropTypes.number.isRequired,
+    event: eventPropTypes,
     fetchEnrolls: PropTypes.func.isRequired,
     enrolls: PropTypes.array.isRequired,
     removeEventEnroll: PropTypes.func.isRequired
@@ -100,6 +162,7 @@ export class ParticipantPage extends PureComponent {
 }
 
 const mapStateToProps = (state, ownProps) => ({
+  event: findEventById(state, ownProps.eventId),
   enrolls: findEventEnrollsByEventId(state, ownProps.eventId) || []
 
 })
