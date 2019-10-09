9c2f79cdf84bb65fc9834335c18aec3d58e860dc
Konsta Purtsi <kovipu@utu.fi>
2019-10-09 11:08:53 +0300

    Revert changes made to allow for 1. Iteraatio page.

--- src/containers/SiteNavbar.js
+++ src/containers/SiteNavbar.js
@@ -2,31 +2,14 @@ import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { Navbar, NavbarMenu, NavbarEnd } from 'bloomer'
 
-const SiteNavbar = ({ brand, isActive, children, onNavbarClick }) => {
-  useEffect(() => {
-    const pathname = document.location.pathname
-    const el = document.getElementById('root')
-    if(pathname.startsWith('/iteraatio')) {
-      el.classList.add('vujut')
-    } else {
-      el.classList.remove('vujut')
-    }
-    if(pathname === '/ilmo/28' || pathname === '/ilmo/29') {
-      el.classList.add('vujuilmo')
-    } else {
-      el.classList.remove('vujuilmo')
-    }
-  })
-
-  return (
-    <Navbar className='has-shadow'>
-      {brand}
-      <NavbarMenu isActive={isActive} onClick={onNavbarClick}>
-        <NavbarEnd>{children}</NavbarEnd>
-      </NavbarMenu>
-    </Navbar>
-  )
-}
+const SiteNavbar = ({ brand, isActive, children, onNavbarClick }) => (
+  <Navbar className='has-shadow'>
+    {brand}
+    <NavbarMenu isActive={isActive} onClick={onNavbarClick}>
+      <NavbarEnd>{children}</NavbarEnd>
+    </NavbarMenu>
+  </Navbar>
+)
 
 SiteNavbar.propTypes = {
   brand: PropTypes.oneOfType([
