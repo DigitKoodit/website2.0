9be368b643b0d5dcdbd0fc4d442e9c0436ecc90a
niemisami <sami@nieminen.fi>
2020-11-02 21:42:35 +0200

    Fetch files only when requested

--- src/containers/Intra/FileManager/FileChooser.js
+++ src/containers/Intra/FileManager/FileChooser.js
@@ -1,44 +1,43 @@
-import React, { PureComponent } from 'react'
+import React, { useRef } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { fileActions } from '../../../actions'
 import { ChooserModal } from '../../../components/Modal'
 
-export class FileChooser extends PureComponent {
-  chooserRef = React.createRef()
-
-  componentDidMount() {
-    this.props.fetchFiles() // TODO: fetch when opened
-  }
-
-  render() {
-    const { files, onSelect } = this.props
-    return (
-      <ChooserModal
-        ref={this.chooserRef}
-        modalTitle='Valitse liite'
-        placeholder='Liitä kuva tai tiedosto'
-        dataSet={files}
-        listItemFormatter={item => <>
+const FileChooser = ({ fetchFiles, files, isLoading, onSelect }) => {
+  const chooserRef = useRef()
+  console.log(isLoading, files)
+  return (
+    <ChooserModal
+      ref={chooserRef.current}
+      modalTitle='Valitse liite'
+      placeholder='Liitä kuva tai tiedosto'
+      dataSet={files}
+      isLoading={isLoading}
+      onOpen={fetchFiles}
+      listItemFormatter={item => (
+        <>
           <img width='50' src={'/' + item.path} alt={item.filename} />
           <small>{item.filename}</small>
-        </>}
-        onSelect={file => {
-          onSelect(file)
-          this.chooserRef.current && this.chooserRef.current.closeModal()
-        }}
-      />
-    )
-  }
-
-  static propTypes = {
-    files: PropTypes.array.isRequired,
-    fetchFiles: PropTypes.func.isRequired,
-    onSelect: PropTypes.func.isRequired
-  }
+        </>)}
+      onSelect={file => {
+        onSelect(file)
+        this.chooserRef.current && this.chooserRef.current.closeModal()
+      }}
+    />
+  )
 }
+
+FileChooser.propTypes = {
+  files: PropTypes.array.isRequired,
+  fetchFiles: PropTypes.func.isRequired,
+  onSelect: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool
+}
+
 const mapStateToProps = (state) => ({
-  files: state.files.records
+  files: state.files.records,
+  isLoading: state.files.loading
 })
 
 const mapDispatchToProps = dispatch => ({
